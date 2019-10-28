78ae4e739f475e19241ebd46fb7c3e7803b123f1
EIDOSO <wericjohn280@gmail.com>
2019-10-29 00:06:12 +0500

    update interface

--- src/qt/overviewpage.h
+++ src/qt/overviewpage.h
@@ -79,6 +79,19 @@ private Q_SLOTS:
     void updateAlerts(const QString &warnings);
     void updateWatchOnlyLabels(bool showWatchOnly);
     void handleOutOfSyncWarningClicks();
+    void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+    void on_pushButton_6_clicked();
+    void on_pushButton_7_clicked();
+    void on_pushButton_8_clicked();
+    void on_pushButton_9_clicked();
+    void on_pushButton_10_clicked();
+    void on_pushButton_11_clicked();
+    void on_pushButton_12_clicked();
+    void on_pushButton_13_clicked();
 };
 
 #endif // BITCOIN_QT_OVERVIEWPAGE_H
