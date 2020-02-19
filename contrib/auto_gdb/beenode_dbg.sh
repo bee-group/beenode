6b5960a884bdd0e4d9daa5615468028e56f550e4
Ваше Имя <you@example.com>
2020-02-19 10:08:59 +0500

    del to new upload

--- contrib/auto_gdb/beenode_dbg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# use testnet settings,  if you need mainnet,  use ~/.beenodecore/beenoded.pid file instead
-beenode_pid=$(<~/.beenodecore/testnet3/beenoded.pid)
-sudo gdb -batch -ex "source debug.gdb" beenoded ${beenode_pid}
