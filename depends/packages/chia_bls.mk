c3e7fd722a36cc7ccd394bc894bdacd0338627a7
BeeGroup <47412305+bee-group@users.noreply.github.com>
2023-07-04 18:57:08 +0300

    Update chia_bls.mk

--- depends/packages/chia_bls.mk
+++ depends/packages/chia_bls.mk
@@ -1,9 +1,9 @@
 package=chia_bls
 $(package)_version=v20181101
 # It's actually from https://github.com/Chia-Network/bls-signatures, but we have so many patches atm that it's forked
-$(package)_download_path=https://github.com/codablock/bls-signatures/archive
+$(package)_download_path= https://github.com/bee-group/bls/blob/main
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=b3ec74a77a7b6795f84b05e051a0824ef8d9e05b04b2993f01040f35689aa87c
+$(package)_sha256_hash=5501610e9cfa7b5d7a9cd95494837115943cc9adef8b85c930b5565736e4fa0f
 $(package)_dependencies=gmp
 #$(package)_patches=...TODO (when we switch back to https://github.com/Chia-Network/bls-signatures)
 
